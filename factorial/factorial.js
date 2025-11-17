85e368a294b914aa33e740fb8c2c61252bda5d09
praveena415 <chinthapraveena194@gmail.com>
2025-11-17 17:58:06 +0530

    done

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -2,5 +2,4 @@ function calculateFactorial(num) {
 	if (num === 0 || num === 1) return 1;
 	return num * calculateFactorial(num - 1);
 }
-
 module.exports = calculateFactorial;
--- factorial/index.js
+++ factorial/index.js
@@ -1,5 +1,4 @@
 const findFactorial = require("./factorial");
-
 let numbers = [5, 7, 10];
 for (let index = 0; index < numbers.length; index++) {
 	let result = findFactorial(numbers[index]);
